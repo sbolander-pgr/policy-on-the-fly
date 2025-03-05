c0c0470c7734b395a880b5ba8dae9271866eb2d8
Seth Bolander <sbolander@e-ins.net>
2025-03-05 13:12:41 -0500

    methods for creating new quote

---
+++ createNewQuote.js
@@ -0,0 +1,44 @@
+const {
+  handleCustomerPage,
+  handleBusinessDetailsPage,
+  handlePropertyDetailsPage,
+  handleCoveragePage,
+  handleSummaryPage,
+  handleFinalDetailsPage,
+  handlePurchasePage,
+  handleSoldQuotePage,
+} = require("./handleQuote");
+
+async function handleStartNewQuoteModal(page) {
+  // click on the "Options" hamburger menu
+  await page.waitForSelector("#btnOptionsMenu", { visible: true });
+  await page.click("#btnOptionsMenu");
+
+  // click on the "Start a new quote" button
+  await page.waitForSelector("#btnNewQuote", { visible: true });
+  await page.click("#btnNewQuote");
+
+  // fill out modal and press "Start New Quote"
+  await page.waitForSelector("#ddlNewPolicyState", { visible: true });
+  await page.select("#ddlNewPolicyState", "OH");
+  await page.type("#ddlAgent", "PROGRESSIVE TEST: 43786 - [Test Agent]");
+  await page.click("#modalCreate .btn-primary");
+}
+
+async function createNewQuote(browser) {
+  const page = await browser.newPage();
+  await page.goto(process.env.BASE_URL);
+  await handleStartNewQuoteModal(page);
+  await handleCustomerPage(page);
+  await handleBusinessDetailsPage(page);
+  await handlePropertyDetailsPage(page);
+  await handleCoveragePage(page);
+  await handleSummaryPage(page);
+  await handleFinalDetailsPage(page);
+  await handlePurchasePage(page);
+  const policyId = await handleSoldQuotePage(page);
+  await page.close();
+  return policyId;
+}
+
+module.exports = { createNewQuote };
